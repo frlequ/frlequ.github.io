495f2823fbcfbaddc82ed3b593c686c6995ff0a0
frlequ <quartstudio@gmail.com>
2024-02-19 13:36:49 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,8 @@
     var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
     initHeader();
     initAnimation();
-    addListeners();
+    //addListeners();
 	
-	/*var posy = 0;*/
 	
     function initHeader() {
         width = window.innerWidth;
@@ -66,6 +65,7 @@
             points[i].circle = c;
         }
     }
+	/*
     function addListeners() {
         if (!('ontouchstart' in window)) {
             window.addEventListener('mousemove', mouseMove);
@@ -73,6 +73,8 @@
         window.addEventListener('scroll', scrollCheck);
         window.addEventListener('resize', resize);
     }
+	
+	*/
     function mouseMove(e) {
         var posx = posy = 0;
         if (e.pageX || e.pageY) {
@@ -85,6 +87,7 @@
         target.x = posx;
         target.y = posy;
     }
+	
     function scrollCheck() {
         if (document.body.scrollTop > height)
             animateHeader = false;
@@ -167,27 +170,176 @@
     function getDistance(p1, p2) {
         return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
     }
-}());//@ sourceURL=pen.js
+}());
 
-/*
-// Function to update the height of .cover elements
-function updateCoverHeight() {
-  // Get the height of the window (viewport)
-  var screenHeight = window.innerHeight;
+//------------------------------------------------------------------------------
 
-  // Find all elements with the class .cover
-  var covers = document.querySelectorAll('.cover');
+// Listen to the scroll event
+window.addEventListener('scroll', function() {
+    var scrolledHeight = window.pageYOffset;
 
-  // Loop through the NodeList of .cover elements
-  covers.forEach(function(cover) {
-    // Set each .cover element's height to match the screen height
-    cover.style.height = screenHeight + 'px';
-  });
+    var cover = document.querySelector('.cover');
+    var cover2 = document.querySelector('.cover2');
+
+    // Calculate the offset position of each element
+    var coverOffset = getOffsetTop(cover);
+    var cover2Offset = getOffsetTop(cover2);
+
+    // Adjust the background position
+    var coordsCover = '50%' + calculatePosition(scrolledHeight, coverOffset) + 'px';
+    cover.style.backgroundPosition = coordsCover;
+
+    var coordsCover2 = '50%' + calculatePosition(scrolledHeight, cover2Offset+120) + 'px';
+    cover2.style.backgroundPosition = coordsCover2;
+});
+
+// Function to calculate an element's top offset
+function getOffsetTop(elem) {
+    var offsetTop = 0;
+    do {
+        if (!isNaN(elem.offsetTop)) {
+            offsetTop += elem.offsetTop;
+        }
+    } while(elem = elem.offsetParent);
+    return offsetTop;
 }
 
-// Call the function to update the height on initial load
-updateCoverHeight();
+// Function to calculate background position
+function calculatePosition(scrolledHeight, elementOffset) {
+    // Adjust the multiplier (-0.5) based on desired parallax speed
+    return (scrolledHeight - elementOffset) * +0.5;
+}
 
-// Optionally, update the height whenever the window is resized
-window.addEventListener('resize', updateCoverHeight);
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    var screenHeight = window.innerHeight;
+    var divWithBackground = document.querySelector('.cover');
+    divWithBackground.style.height = screenHeight + 'px';
+});
+
 */
+
+
+
+//------------------------------------------------------------------------------
+
+
+
+
+       // Step 3: JavaScript to initialize the chart
+		
+		var apexConfig = {
+			
+			colors: ['var(--primary-chart-back)'],
+			
+			plotOptions: { 
+			  bar: { 
+				columnWidth: '100%' 
+			  } 
+			},
+		  chart: {
+		    type: 'bar', // Change to 'line', 'area', etc. for different chart types
+            
+
+			height: '180px',
+			width: '100%',
+			animations: {
+			  enabled: false
+			},
+			toolbar: {
+				show: false
+			}
+		  },
+		  dataLabels: {
+			enabled: false,
+			offsetY: -8,
+			style: {
+			  fontSize: '10px',
+			  fontFamily: 'Segoe UI'
+			},
+			background: {
+			  enabled: false
+			}
+		  },
+		  tooltip: {
+			enabled: false,
+			followCursor: true,
+			fixed: {
+			  enabled: true,
+			  position: 'topLeft',
+			  offsetY: 180
+			},
+			x: {
+			  format: 'dddd, d MMMM'
+			},
+
+			items: {
+			  display: 'flex'
+			}
+		  },
+
+		  stroke: {
+			colors: ['transparent'],
+			width: 1
+		  },
+		  xaxis: {
+			floating: true,
+			labels: {
+			  show: false
+			},
+			axisTicks: {
+			  show: false
+			},
+			axisBorder: {
+			  show: false
+			},
+			tooltip: {
+			  enabled: false
+			},
+			crosshairs: {
+			  show: true,
+			  width: 25,
+			  position: 'front',
+			  opacity: 0.2
+			}
+		  },
+		  yaxis: {
+			min: 0,
+			decimalsInFloat: 2,
+			tickAmount: 2,
+			floating: true,
+			labels: {
+			  show: true,
+			  offsetX: 31,
+			  offsetY: -6
+			}
+		  },
+		  grid: {
+			strokeDashArray: 2,
+			borderColor: '#666666',
+			padding: {
+			  left: 0,
+			  right: 0
+			}
+		  },
+		  series: [{
+                name: 'kWh Energy',
+                data: [14.26, 14.80, 16.92, 12.59, 12.23, 11.58, 11.93, 13.88, 11.82] // Random kWh data
+			}],
+		};
+
+        var options2 = {
+
+			series: [{
+				name: 'kWh Energy',
+				data: Array.from({ length: 96 }, () => parseFloat((Math.random() * (100 - 10) + 10).toFixed(2)))
+			}]
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), apexConfig );
+        chart.render();
+		
+		apexConfig.series = options2.series;
+		
+		var chart = new ApexCharts(document.querySelector("#chart2"), apexConfig );
+        chart.render();
